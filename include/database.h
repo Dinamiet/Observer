a251e1e8ae798d0098d2dbf0e2e255ac3ad584c2
Dinamiet <iceman.sm39@gmail.com>
2021-05-22 14:52:16 +0200

    Function layout

---
+++ include/database.h
@@ -0,0 +1,10 @@
+#ifndef __DATABASE_H__
+#define __DATABASE_H__
+
+void Database_Init();
+void Database_Subscribe();
+void Database_FindSubscription();
+void Database_Unsubscribe();
+void Database_Notify();
+
+#endif
---
+++ src/database.c
@@ -0,0 +1,11 @@
+#include "database.h"
+
+void Database_Init() {}
+
+void Database_Subscribe() {}
+
+void Database_FindSubscription() {}
+
+void Database_Unsubscribe() {}
+
+void Database_Notify() {}
