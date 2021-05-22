f97e25a1b253470ba2fe4c0f66156204b7e94d30
Dinamiet <iceman.sm39@gmail.com>
2021-05-22 15:16:29 +0200

    Database functions implemented

--- include/database.h
+++ include/database.h
@@ -1,10 +1,27 @@
 #ifndef __DATABASE_H__
 #define __DATABASE_H__
 
-void Database_Init();
-void Database_Subscribe();
-void Database_FindSubscription();
-void Database_Unsubscribe();
-void Database_Notify();
+#include "bufferedlist.h"
+
+#include <stdint.h>
+
+typedef struct
+{
+	Node	 List;
+	uint32_t Name;
+	uint32_t Topic;
+	void (*Callback)(void*);
+} Subscription;
+
+typedef struct
+{
+	BufferedList Subscriptions;
+} Database;
+
+void		  Database_Init(Database* database, Subscription* buffer, uint32_t number);
+Subscription* Database_Subscribe(Database* database, char* name, char* topic, void (*callback)(void*));
+Subscription* Database_FindSubscription(Database* database, char* name);
+void		  Database_Unsubscribe(Database* database, Subscription* subscription);
+void		  Database_Notify(Database* database, char* topic, void* data);
 
 #endif
--- src/database.c
+++ src/database.c
@@ -1,11 +1,60 @@
 #include "database.h"
 
-void Database_Init() {}
+// #include "fnv.h"
+// #define HASH_FUNC FNV_Hash
 
-void Database_Subscribe() {}
+#include "sdbm.h"
+#define HASH_FUNC SDBM_Hash
 
-void Database_FindSubscription() {}
+#include <string.h>
 
-void Database_Unsubscribe() {}
+void Database_Init(Database* database, Subscription* buffer, uint32_t number)
+{
+	BufferedList_Init(&database->Subscriptions, (Node*)buffer, sizeof(Subscription), number);
+}
 
-void Database_Notify() {}
+Subscription* Database_Subscribe(Database* database, char* name, char* topic, void (*callback)(void*))
+{
+	Subscription* sub = (Subscription*)BufferedList_LinkTail(&database->Subscriptions);
+	if (sub)
+	{
+		sub->Name	  = HASH_FUNC((uint8_t*)name, strlen(name));
+		sub->Topic	  = HASH_FUNC((uint8_t*)topic, strlen(name));
+		sub->Callback = callback;
+	}
+
+	return sub;
+}
+
+Subscription* Database_FindSubscription(Database* database, char* name)
+{
+	Subscription* sub	  = (Subscription*)database->Subscriptions.Used.Head;
+	uint32_t	  subName = HASH_FUNC((uint8_t*)name, strlen(name));
+	if (sub == NULL)
+		return NULL;
+	do {
+		if (sub->Name == subName)
+			return sub;
+
+		sub = (Subscription*)sub->List.Next;
+	} while (sub != (Subscription*)database->Subscriptions.Used.Head);
+
+	// Subscription could not be found
+	return NULL;
+}
+
+void Database_Unsubscribe(Database* database, Subscription* subscription) { BufferedList_UnlinkNode(&database->Subscriptions, &subscription->List); }
+
+void Database_Notify(Database* database, char* topic, void* data)
+{
+	Subscription* sub		= (Subscription*)database->Subscriptions.Used.Head;
+	uint32_t	  topicName = HASH_FUNC((uint8_t*)topic, strlen(topic));
+	if (sub == NULL)
+		return;
+	do {
+		if (sub->Topic == topicName)
+			sub->Callback(data);
+
+		sub = (Subscription*)sub->List.Next;
+	} while (sub != (Subscription*)database->Subscriptions.Used.Head);
+}
